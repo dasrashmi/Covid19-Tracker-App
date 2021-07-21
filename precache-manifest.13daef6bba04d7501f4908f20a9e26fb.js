bf1b534b74f1b79bcea293a2c97f4c058376546a
Rashmi <das.rcew21@gmail.com>
2021-07-21 20:16:59 +0530

    Updates

---
+++ precache-manifest.13daef6bba04d7501f4908f20a9e26fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7db94d029874ce09e10d42199e693ab",
+    "url": "/dasrashmi/Covid19-Tracker-App.git/index.html"
+  },
+  {
+    "revision": "c687faa65852079a8011",
+    "url": "/dasrashmi/Covid19-Tracker-App.git/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e4293f584cc79494873c",
+    "url": "/dasrashmi/Covid19-Tracker-App.git/static/css/main.92943667.chunk.css"
+  },
+  {
+    "revision": "c687faa65852079a8011",
+    "url": "/dasrashmi/Covid19-Tracker-App.git/static/js/2.e7e47166.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/dasrashmi/Covid19-Tracker-App.git/static/js/2.e7e47166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4293f584cc79494873c",
+    "url": "/dasrashmi/Covid19-Tracker-App.git/static/js/main.ce768bf6.chunk.js"
+  },
+  {
+    "revision": "7efab4f2c74eabea24d1",
+    "url": "/dasrashmi/Covid19-Tracker-App.git/static/js/runtime-main.cf788f10.js"
+  }
+]);
